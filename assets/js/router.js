a5965343d0a42a15fdd57ed58aa9d7f26270ec8a
dishantsethi <dishantsethi14@gmail.com>
2023-12-09 23:14:09 +0530

    inital commit

---
+++ assets/js/router.js
@@ -0,0 +1,24 @@
+onst route = (event) => {
+    event = event || window.event
+    event.preventDefault();
+    console.log(event)
+    window.history.pushState({}, "", event.target.href)
+    handleLocation();
+}
+
+const routes = {
+    404: "/pages/404.html",
+    "/careers": "/pages/careers.html",
+    "/privacy-policy": "/pages/privacy-policy.html",
+    "/terms-and-conditions": "/pages/terms-and-conditions.html",
+}
+
+const handleLocation = async () => {
+    const path = window.location.pathname
+    const route = routes[path] || routes[404]
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html
+    scroll(500, 500)
+};
+
+window.route = route
